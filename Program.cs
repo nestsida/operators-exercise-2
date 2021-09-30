0dd86753d67900e16e971952db95ddc6ab8b00ba
sarah sida <nestsida@gmail.com>
2021-09-30 14:24:48 -0400

    final push

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operators_exercise_2
+{
+    public class AreaOfCircle
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is the radius of your circle?");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            double AreaOfCircle = (_ = Math.PI * radius * radius);
+
+            Console.WriteLine("Area Of Circle:" + AreaOfCircle);
+        }
+
+        public static double Calculate(double variable)
+        {
+            Console.Write("What is the radius of your circle?");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            double AreaOfCircle = (_ = Math.PI * radius * radius);
+
+            Console.WriteLine("Area Of Circle:" + AreaOfCircle);
+            return AreaOfCircle;
+
+
+        }
+
+
+
+    }
+}
+
